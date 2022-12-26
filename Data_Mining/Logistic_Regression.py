a84ae786b63ff5a1d25081f20e8355575c5938ad
jeewonkimm2 <jeewoncoding@gmail.com>
2022-12-26 22:32:16 +0900

    Second fix

--- Data_Mining/Logistic_Regression.py
+++ Data_Mining/Logistic_Regression.py
@@ -27,3 +27,55 @@ print(res)
 # 이 값이 3.31이다
 print(np.mean(D))
 # 결론적으로 res == np.mean(D)
+
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets
+import pandas as pd
+import sklearn
+
+# 92 rows, 2 columns(height, sex)
+height = pd.read_csv('https://drive.google.com/uc?export=download&id=1m0noi5t5StwPdTACZOkP22hKKR6v4vLX')
+print(height)
+print(height.shape)
+
+# L2 Regularization(0으로 근사)사용. C값이 낮을수록 계수를 0으로 근사하는 regularization이 강화됨
+# Regularization C값이 클수록 decision boundary는 최대한 train data를 맞추려고 함. 즉 overfitting의 가능성이 증가합니다.
+clf = LogisticRegression(C=10)
+
+# LogisticRegression model training
+clf.fit(height[['height']],height['sex'])
+print(clf.coef_)
+print(clf.intercept_)
+
+clf.predict(height[['height']])
+
+# predict_proba : 각 클래스에 대한 확률
+y_prob = clf.predict_proba(height[['height']])
+print(y_prob)
+
+# axis=1은 y축을 기준으로 row 별로 존재하는 column들의 값을 합쳐 1개로 축소하는 과정
+# 각 확률의 합이 1이란 뜻
+print(np.sum(y_prob,axis=1))
+
+
+iris = datasets.load_iris()
+
+X = iris.data
+y = iris.target
+
+clf2 = LogisticRegression(C=10e5)
+clf2.fit(X,y)
+
+print(clf2.coef_)
+print(clf2.intercept_)
+
+y_pred2 = clf2.predict(X)
+y_prod2 = clf2.predict_proba(X)
+print(y_prod2)
+print(np.sum(y_prod2,axis=1))
+
+print(clf2.score(X,y))
+
+print(np.sum(y_pred2==y)/len(X))
