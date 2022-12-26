6aafdc32c6aad71937c1a091195380f3c04f7057
jeewonkimm2 <jeewoncoding@gmail.com>
2022-12-26 13:42:37 +0900

    petrol.csv 수정

--- Data_Mining/Linear_Regression.py
+++ Data_Mining/Linear_Regression.py
@@ -65,3 +65,85 @@ print("F-Test : ", ftest)
 print(stats.f.ppf(0.95, p, n-p-1))
 # cdf : Cumulative distribution function of F
 print(1-stats.f.cdf(ftest,p,n-p-1))
+
+
+
+# Importing petrol data
+# 49 rows, 5 columns('tax','income','highway','driver','petrol')
+petrol = pd.read_csv('/Users/jeewonkim/Desktop/petrol.csv')
+print(petrol.head())
+print(petrol.dtypes)
+
+
+
+# Data Training
+reg.fit(petrol[['tax','income','highway','license']],petrol['consumption'])
+
+print("Coefficients of petrol :", reg.coef_)
+print("Intercept of petrol :", reg.intercept_)
+
+from numpy import linalg
+X = petrol[['tax','income','highway','license']].values
+
+# [1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1.]
+print(np.ones((13,)))
+# [[1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]
+#  [1. 1.]]
+print(np.ones((12,2)))
+
+# 가로 방향으로 두 배열 합치기
+# 첫번째 column에 1추가 후 X와 합침
+X1 = np.c_[np.ones(len(X)),X]
+print(X1)
+print(X1.shape, X1.T.shape)
+
+
+A = np.array([[1,2],[3,4]])
+B = np.array([[5,6],[7,8]])
+print(A*B)
+
+# X1 transpose * X1
+XtX = np.matmul(X1.T, X1)
+print(XtX.shape)
+
+inv_XtX = linalg.inv(XtX)
+I = np.matmul(XtX, inv_XtX)
+
+# 한꺼번에 intercept와 coefficients구하는 방법
+beta = np.matmul(np.matmul(inv_XtX,X1.T),petrol[['consumption']].values)
+# 숫자가 동일한지 확인하는 과정
+print(beta[0], reg.intercept_)
+print(beta[1], reg.coef_[0])
+
+y_true = petrol['consumption'].values
+y_pred = reg.predict(petrol[['tax','income','highway','license']])
+
+SST = np.sum((y_true-np.mean(y_true))**2)
+SSR = np.sum((y_pred-np.mean(y_true))**2)
+SSE = np.sum((y_true-y_pred)**2)
+
+print(SST, SSR+SSE)
+
+p = 4
+n = petrol.shape[0]
+
+MSR = SSR/p
+MSE = SSE/(n-p-1)
+print(MSR, MSE)
+
+ftest = MSR/MSE
+print(ftest)
+
+from scipy import stats
+print(stats.f.ppf(0.95, p, n-p-1))
+print(1-stats.f.cdf(ftest,p,n-p-1))
